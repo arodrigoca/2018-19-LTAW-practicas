8d2a6cd7506c08bb9540e451cb18c7f36f3ed1cd
arodrigoca <adri_rodri_64@hotmail.es>
2019-02-10 21:34:54 +0100

    update

--- Practica-1/compact_server/compact_server.js
+++ Practica-1/compact_server/compact_server.js
@@ -13,27 +13,26 @@ const mime = {
 
 console.log('Starting server...');
 server = http.createServer(request_handler);
-server.listen(8080);
+server.listen(8888);
 
 function  send_response(req, res){
 
     var q = url.parse(req.url, true); //retrieve url
-    var filename = "." + q.pathname;  //retrieve filename
+    var filename = "static" + q.pathname;  //retrieve filename
 
-    fs.readFile(filename, 'utf8', (err, data) => {
+    fs.readFile(filename, (err, data) => {
         if(err == null){
             console.log('client requested resource:', filename, 'and it was found');
-            var extension = filename.split(".")[2];
-            var mimeType = mime[extension];
+            let mimeType =  mime[filename.split(".")[1]];
             console.log(mimeType);
             res.writeHead(200, {'Content-Type': mimeType});
             res.write(data);
             res.end();
 
         }else{
-            if(filename == "./" || filename == ""){
+            if(filename == "/" || filename == ""){
               console.log("Client didn't request anything. Sending index");
-              fs.readFile("./index.html", 'utf8', (err,data)=>{
+              fs.readFile("static/index.html", 'utf8', (err,data)=>{
                 res.writeHead(200, {'Content-Type': 'text/html'});
                 res.write(data);
                 res.end();
